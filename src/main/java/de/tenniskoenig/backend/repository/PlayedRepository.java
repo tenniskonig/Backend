ccb8781567c6f752ba581ae5bcf0f23bc998e34e
Chsldz <36513140+Chsldz@users.noreply.github.com>
2019-04-25 13:28:24 +0200

    TEN-100 - API send matches per player

--- src/main/java/de/tenniskoenig/backend/repository/PlayedRepository.java
+++ src/main/java/de/tenniskoenig/backend/repository/PlayedRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface PlayedRepository extends JpaRepository<Played, Long> {
-    @Query("select p.userID, sum(p.points)from Played p group by p.userID order by p.userID asc")
+    @Query("select p.userID, sum(p.points), count(p) from Played p group by p.userID order by p.userID asc")
     Iterable<Played> getHighscore();
 }
