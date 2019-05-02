0d5b42d858875c104912294fd5a3d4c48effdb64
Chsldz <36513140+Chsldz@users.noreply.github.com>
2019-05-02 16:23:13 +0200

    TEN-114 - Test all GET Player

--- src/test/java/de/tenniskoenig/backend/BackendApplicationTests.java
+++ src/test/java/de/tenniskoenig/backend/BackendApplicationTests.java
@@ -35,13 +35,27 @@ public class BackendApplicationTests {
 
 
 	@Test
-	public void player(){
+	public void playerAll(){
 		given().auth().oauth2(accessToken)
 				.get("/api/player")
 				.then().statusCode(200);
-}
+	}
 
+	@Test
+	public void playerId(){
+		given().auth().oauth2(accessToken)
+				.get("/api/player/id/1")
+				.then().statusCode(200);
+	}
 
+	Response player;
+	@Test
+	public void player(){
+		player = given().auth().oauth2(accessToken)
+				.get("/api/player/name/sven.haala");
+		assertEquals(player.getStatusCode(), 200);
+	}
+	
 	private boolean login(String username, String password) {
 		logger.info("Port is: "+ port);
 		logger.info("Getting OAuth Token from server - {}"+ RestAssured.baseURI);
